06d2d165a5e4572627ff4ef72f1f107efc53ab0a
RazEfron <razefron01@gmail.com>
2024-02-07 19:43:12 -0500

    add prompts

---
+++ index.js
@@ -0,0 +1,23 @@
+// Sample Module 1 Assessment:
+
+// 1. Given a string of two words, both made of only lowercase letters, write a function that returns the same string, with the first letter of each word capitalized
+
+// 'mickey mouse' => 'Mickey Mouse'
+// 'kiera gonzalez' => 'Kiera Gonzalez'
+
+// 2. Write a function that takes an array of lowercase words, return the words that have one or more 'e' but do not start with 'e'.
+
+// ["ellen", "jane", "zigland", "pedestrian"] => ["jane", "pedestrian"]
+
+// ['red', 'lindsay', 'equine', 'drip'] => ['red'] 
+
+// 3. Write a function that takes a string of lowercase letters, and returns an object with a count of how many times each letter appears
+
+// 'asciiaske' => {
+//   'a' : 2, 
+//   's' : 2,
+//   'c' : 1,
+//   'i' : 2,
+//   'k' : 1, 
+//   'e' : 1
+// }
